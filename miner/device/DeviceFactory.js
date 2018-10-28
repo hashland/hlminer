506fd5f855023bf826852e99afa5c6ca56932c52
Julian Haupt <julian.haupt@hauptmedia.de>
2018-10-28 22:28:43 +0100

    use node-usb implementation again as fixes have been upstreamed

--- miner/device/DeviceFactory.js
+++ miner/device/DeviceFactory.js
@@ -1,20 +1,26 @@
 const
-    { UsbManager }                          = require('./usb/UsbManager'),
-    { BAIKAL_ID_VENDOR, BAIKAL_ID_PRODUCT } = require('./baikal/constants'),
-    { BaikalUsbDevice }                     = require('./baikal/BaikalUsbDevice'),
-    { CpuDevice }                           = require('./CpuDevice');
+    usb = require('usb'),
+    {BAIKAL_ID_VENDOR, BAIKAL_ID_PRODUCT} = require('./baikal/constants'),
+    {BaikalUsbDevice} = require('./baikal/BaikalUsbDevice'),
+    {CpuDevice} = require('./CpuDevice');
 
 class DeviceFactory {
     static createAvailableDevices(createCpuDevice) {
         let devices = [];
 
-        const baikalDevices = UsbManager.findAllByIds(BAIKAL_ID_VENDOR, BAIKAL_ID_PRODUCT);
-        devices = devices.concat(baikalDevices.map(usbDevice => new BaikalUsbDevice(usbDevice)));
+        const usbDevices = usb.getDeviceList(),
+            baikalDevices = usbDevices
+                .filter(
+                    device => device.deviceDescriptor.idVendor === BAIKAL_ID_VENDOR &&
+                        device.deviceDescriptor.idProduct === BAIKAL_ID_PRODUCT
+                )
+                .map(usbDevice => new BaikalUsbDevice(usbDevice));
 
-        if(createCpuDevice) {
+        devices = devices.concat(baikalDevices);
+
+        if (createCpuDevice) {
             devices.push(new CpuDevice());
         }
-
 
         return devices;
     }
